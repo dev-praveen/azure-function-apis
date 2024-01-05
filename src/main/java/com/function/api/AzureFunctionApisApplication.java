4d7788dfbfd56683aeffb13f2c70483ca1adb5ce
dev-praveen <spraveen0244@gmail.com>
2024-01-05 21:15:49 +0530

    initial project setup for azure function web apis

---
+++ src/main/java/com/function/api/AzureFunctionApisApplication.java
@@ -0,0 +1,13 @@
+package com.function.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureFunctionApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureFunctionApisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/function/api/AzureFunctionApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.function.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureFunctionApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
